9c3609e365df75c934f104440f9c8f46e2105b91
Christopher Miller <admin@mctherealm.net>
2018-08-10 16:41:46 -0400

    Changed logged out home page to be login page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,11 @@
 class PagesController < ApplicationController
   skip_authorization_check
+
+  def root
+    if user_signed_in?
+      render file: 'pages/dashboard'
+    else
+      redirect_to new_user_session_path
+    end
+  end
 end
