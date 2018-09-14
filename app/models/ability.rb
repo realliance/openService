b38db090a834e8f314c08583959ca2eeba638ceb
Christopher Miller <admin@mctherealm.net>
2018-09-14 12:22:01 -0400

    Fixed bug with CanCanCan not triggering checks due to controller differences

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -12,10 +12,6 @@ class Ability
     can :view_list, Participant
     can %i[create destroy], Participant, user_id: user.id
 
-    can %i[index update sign_in_sheet], Participant do |participant|
-      participant.event.user == user
-    end
-
     return unless user.admin?
     # If User is at least an Admin
     can :manage, :all
